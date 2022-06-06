dc19cfdd9f0eefce741cf2549cef8bd0195a097b
Stavros <snikoletopoulos@hotmail.com>
2022-06-06 03:03:12 +0300

    Register resolvers to apollo server

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
 import { ApolloServer } from "apollo-server";
+import { PrismaClient } from "@prisma/client";
 
 import { typeDefs } from "./schema";
-import { Query } from "./resolvers";
+import { Query, Mutation } from "./resolvers";
+
+const prisma = new PrismaClient();
+
+export interface Context {
+	prisma: PrismaClient;
+}
 
 const server = new ApolloServer({
 	typeDefs,
 	resolvers: {
 		Query,
+		Mutation,
 	},
+	context: {
+		prisma,
+	} as Context,
 });
 
 server.listen().then(({ url }) => {
