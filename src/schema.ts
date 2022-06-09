8b433462d5a53c27284860d92915293ea3dd534c
Stavros <snikoletopoulos@hotmail.com>
2022-06-10 02:40:54 +0300

    Remove duplicate schema

--- src/schema.ts
+++ src/schema.ts
@@ -46,36 +46,4 @@ export const typeDefs = gql`
 		title: String
 		content: String
 	}
-
-	type Post {
-		id: ID!
-		title: String!
-		content: String!
-		createdAt: String!
-		published: Boolean!
-		user: User!
-	}
-
-	type User {
-		id: ID!
-		name: String!
-		email: String!
-		profile: Profile!
-		posts: [Post!]!
-	}
-
-	type Profile {
-		id: ID!
-		bio: String!
-		user: User!
-	}
-
-	type UserError {
-		message: String!
-	}
-
-	type PostPayload {
-		userErrors: [UserError!]!
-		post: Post
-	}
 `;
