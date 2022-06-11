d93f13945c0634a35d7aabcdcaee5c7a040c507a
Stavros <snikoletopoulos@hotmail.com>
2022-06-11 21:41:15 +0300

    Fetch and display the posts

--- client/src/pages/Posts/Posts.tsx
+++ client/src/pages/Posts/Posts.tsx
@@ -1,7 +1,46 @@
-import Post from "components/Post/Post";
+import { gql, useQuery } from "@apollo/client";
+
+import Post from "components/Post";
+
+const GET_POSTS = gql`
+	query {
+		posts {
+			id
+			title
+			content
+			createdAt
+			user {
+				name
+			}
+		}
+	}
+`;
 
 const Posts: React.FC = () => {
-	return <div></div>;
+	const { loading, error, data } = useQuery(GET_POSTS);
+
+	if (error) return <div>Error Page</div>;
+
+	if (loading) return <div>Loading...</div>;
+
+	const { posts } = data;
+
+	return (
+		<div>
+			{posts.map(post => (
+				<Post
+					key={post.id}
+					id={post.id}
+					title={post.title}
+					content={post.content}
+					date={post.createdAt}
+					user={post.user}
+					published={post.published}
+					isMyProfile={false}
+				/>
+			))}
+		</div>
+	);
 };
 
 export default Posts;
