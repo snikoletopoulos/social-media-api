b6c96465be75fef2683d49bd204046925076b880
Stavros <snikoletopoulos@hotmail.com>
2022-06-15 01:35:25 +0300

    Add mutation to publish and unpublish posts

--- client/src/components/Post/Post.tsx
+++ client/src/components/Post/Post.tsx
@@ -1,4 +1,34 @@
 import styles from "./Post.module.css";
+import { gql, useMutation } from "@apollo/client";
+import { ResponsePayload } from "types/response.types";
+
+import { PublusityVariables } from "./Post.types";
+
+const PUBLISH_POST = gql`
+	mutation PublishPost($postId: ID!) {
+		postPublish(postId: $postId) {
+			userErrors {
+				message
+			}
+			post {
+				title
+			}
+		}
+	}
+`;
+
+const UNPUBLISH_POST = gql`
+	mutation UnpublishPost($postId: ID!) {
+		postUnpublish(postId: $postId) {
+			userErrors {
+				message
+			}
+			post {
+				title
+			}
+		}
+	}
+`;
 
 interface Props {
 	id: number;
@@ -14,8 +44,32 @@ interface Props {
 
 const Post: React.FC<Props> = props => {
 	const { title, content, date, user, published, isMyProfile } = props;
+	const [publishPost] = useMutation<
+		ResponsePayload<{ title: string }>,
+		PublusityVariables
+	>(PUBLISH_POST);
+	const [unpublishPost] = useMutation<
+		ResponsePayload<{ title: string }>,
+		PublusityVariables
+	>(UNPUBLISH_POST);
 
 	const formatedDate = new Date(+date);
+
+	const togglePublished = () => {
+		if (published) {
+			unpublishPost({
+				variables: {
+					postId: props.id,
+				},
+			});
+		} else {
+			publishPost({
+				variables: {
+					postId: props.id,
+				},
+			});
+		}
+	};
 
 	return (
 		<div
@@ -23,7 +77,7 @@ const Post: React.FC<Props> = props => {
 			style={published === false ? { backgroundColor: "hotpink" } : {}}
 		>
 			{isMyProfile && (
-				<p className={styles["Post__publish"]} onClick={() => {}}>
+				<p className={styles["Post__publish"]} onClick={togglePublished}>
 					{published ? "publish" : "unpublish"}
 				</p>
 			)}
